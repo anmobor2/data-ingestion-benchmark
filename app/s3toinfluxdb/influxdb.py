1d42496e5a09dffdbfbc417600db5bc39b6ee925
anmobor <amoreno@circutor.com>
2022-03-01 00:27:47 +0100

    influxdb done

--- app/s3toinfluxdb/influxdb.py
+++ app/s3toinfluxdb/influxdb.py
@@ -21,22 +21,10 @@ def keys(bucket_name, prefix='/', delimiter='/'):
     bucket = boto3.resource('s3').Bucket(bucket_name)
     return (_.key for _ in bucket.objects.filter(Prefix=prefix))
 
-@click.command()
-@click.option('--s3bucket', help='S3 to get data from')
-@click.option('--prefix', default="", help='S3 prefix')
-def run(s3bucket, prefix):
+def frombucket(s3bucket, prefix, devicesfile, client, write_api):
     s3 = boto3.client('s3')
-
-    client = influxdb_client.InfluxDBClient(
-       url=url,
-       token=token,
-       org=org
-    )
-
-    write_api = client.write_api(write_options=ASYNCHRONOUS)
-
-
     files = keys(s3bucket, prefix)
+
     total = 0
     for item in files:
         print(item)
@@ -44,18 +32,73 @@ def run(s3bucket, prefix):
         s3.download_fileobj(s3bucket, item, buf)
         json_object = json.loads(buf.getvalue().decode("utf-8"))
 
-        p = influxdb_client.Point("energy")
-        for tag, value in json_object['tags'].items():
-            p.tag(tag, value)
+        device_id = json_object["device_id"]
+        if devicesfile:
+            with open(devicesfile, 'r') as filedevices:
+                for line in filedevices:
+                    device = line.replace('\n', '')
+                    print(device.rstrip())
+                    if device == device_id:
 
-        for var, value in json_object['values'].items():
-            p.field(var, value)
-        p.time(json_object['ts'])
-        write_api.write(bucket=idb_bucket, org=org, record=p)
+                        p = influxdb_client.Point("energy")
+                        for tag, value in json_object['tags'].items():
+                            p.tag(tag, value)
 
+                        for var, value in json_object['values'].items():
+                            p.field(var, value)
+                            p.time(json_object['ts'])
+                            write_api.write(bucket=idb_bucket, org=org, record=p)
 
         total = total + 1
     print(total)
+
+def fromfile(filename, devicesfile, client, write_api):
+    if filename:
+        with open(filename, 'r') as f:
+            if filename:
+                with open(filename, 'r') as f:
+                    json_object = json.loads(f.read())
+                    device_id = json_object["device_id"]
+                    tags = json_object["tags"]
+                    if devicesfile:
+                        with open(devicesfile, 'r') as filedevices:
+                            for line in filedevices:
+                                device = line.replace('\n', '')
+                                if device == device_id:
+                                    p = influxdb_client.Point("energy")
+                                    for tag, value in json_object['tags'].items():
+                                        p.tag(tag, value)
+
+                                    for var, value in json_object['values'].items():
+                                        p.field(var, value)
+                                        p.time(json_object['ts'])
+                                        write_api.write(bucket=idb_bucket, org=org, record=p)
+
+
+@click.command()
+@click.option('--s3bucket', help='S3 to get data from')
+@click.option('--prefix', default="", help='S3 prefix')
+@click.option('--filename', default=None)
+@click.option('--devicesfile', default=None)
+@click.option('--url', default="http://localhost:8086")
+def run(s3bucket, prefix, filename, devicesfile, url):
+
+
+    client = influxdb_client.InfluxDBClient(
+       url=url,
+#       token=token,
+#       org=org
+    )
+
+    write_api = client.write_api(write_options=ASYNCHRONOUS)
+
+    if prefix and s3bucket and devicesfile:
+        frombucket(s3bucket, prefix, devicesfile, client, write_api)
+
+    if filename and devicesfile:
+        fromfile(filename, devicesfile, client, write_api)
+
+
 
 
 if __name__ == "__main__":
